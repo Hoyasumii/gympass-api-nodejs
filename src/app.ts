8518755487eab1cfd682b643e87b5b9966020934
Alan Reis <alanreisanjo@gmail.com>
2024-11-19 11:59:05 -0300

    Ajustes finais de ambiente

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import fastify from "fastify";
 import dotenv from "dotenv";
+import { getLogger } from "@/utils";
 
 if (!process.env.NODE_ENV) {
   dotenv.config();
@@ -8,7 +9,7 @@ if (!process.env.NODE_ENV) {
 }
 
 const app = fastify({
-  logger: process.env.NODE_ENV === "development",
+  logger: getLogger(process.env.NODE_ENV),
 });
 
 export default app;
